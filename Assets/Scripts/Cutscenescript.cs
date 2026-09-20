02edc56decf7dfed06abc981ad1d63f6b4824765
yehormuz-web <yehor.muz@gmail.com>
2026-09-19 22:06:30 -0400

    Added music to the game

--- Assets/Scripts/Cutscenescript.cs
+++ Assets/Scripts/Cutscenescript.cs
@@ -4,11 +4,28 @@ using UnityEngine.Playables;
 
 public class Cutscenescript : MonoBehaviour
 {
+    public Cutsceneexclusive cutscenecontrols;
     public PlayableDirector director;
+    private InputAction skipbutton;
     
     public void pausetimeline()
     {
         director.Pause();
+    }
+    private void Awake()
+    {
+        cutscenecontrols = new Cutsceneexclusive();
+    }
+    private void OnEnable()
+    {
+        skipbutton = cutscenecontrols.NextDiaulog.Newaction;
+        skipbutton.Enable();
+        skipbutton.performed+=nextDiaulog;
+    }
+    private void nextDiaulog(InputAction.CallbackContext callback) 
+    {
+        director.Play();
+        Debug.Log("buttonpress");        
     }
     private void Update()
     {
